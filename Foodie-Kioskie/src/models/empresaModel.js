db97f753efbbd844fc7319b491ddec698e040fa4
danielleromano <danielle.romano@sptech.school>
2023-10-19 21:29:02 -0300

    Arrumando web-dat-viz

--- Foodie-Kioskie/src/models/empresaModel.js
+++ Foodie-Kioskie/src/models/empresaModel.js
@@ -1,27 +1,12 @@
-var database = require("../database/config");
+var database = require('../database/config');
 
-function buscarPorId(id) {
-  var query = `select * from empresa where id = '${id}'`;
-
-  return database.executar(query);
+async function cadastrar(empresa){
+    var instrucaoSql = `
+        INSERT INTO empresa (nomeResposavel, nomeFantasia, cnpj, tel, cep, tipoLogradouro, logradouro, numero, complemento, bairro, cidade, fkEstado) VALUES ('${empresa.nomeResponsavel}', '${empresa.nomeFantasia}', '${empresa.cnpj}', '${empresa.tel}', '${empresa.cep}', '${empresa.tipoLogradouro}', '${empresa.logradouro}', '${empresa.numeroLogradouro}', '${empresa.complemento}', '${empresa.bairro}', '${empresa.cidade}', ${empresa.idEstado});
+    `;    
+    await database.executar(instrucaoSql);
 }
 
-function listar() {
-  var query = `select * from empresa`;
-
-  return database.executar(query);
+module.exports = {
+    cadastrar
 }
-
-function buscarPorCnpj(cnpj) {
-  var query = `select * from empresa where cnpj = '${cnpj}'`;
-
-  return database.executar(query);
-}
-
-function cadastrar(razaoSocial, cnpj) {
-  var query = `insert into empresa (razao_social, cnpj) values ('${razaoSocial}', '${cnpj}')`;
-
-  return database.executar(query);
-}
-
-module.exports = { buscarPorCnpj, buscarPorId, cadastrar, listar };
